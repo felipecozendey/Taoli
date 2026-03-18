e9d22aeafce02f17454fc1a806c9901f6c9ccf17
Skip <skip@usecurling.com>
2026-03-18 22:48:08 +0000

    System-Update: Update .gitignore and remove ignored files from tracking

--- dev-dist/assets/chevron-left-CgXgO8lG.js
+++
@@ -1,9 +0,0 @@
-import { ft as createLucideIcon } from "./PageContent-DFaAkjCU.js";
-var ChevronLeft = createLucideIcon("chevron-left", [["path", {
-	d: "m15 18-6-6 6-6",
-	key: "1wnfg3"
-}]]);
-//#endregion
-export { ChevronLeft as t };
-
-//# sourceMappingURL=chevron-left-CgXgO8lG.js.map
--- dev-dist/assets/flame-BaiQWJ2t.js
+++
@@ -1,9 +0,0 @@
-import { ft as createLucideIcon } from "./PageContent-DFaAkjCU.js";
-var Flame = createLucideIcon("flame", [["path", {
-	d: "M12 3q1 4 4 6.5t3 5.5a1 1 0 0 1-14 0 5 5 0 0 1 1-3 1 1 0 0 0 5 0c0-2-1.5-3-1.5-5q0-2 2.5-4",
-	key: "1slcih"
-}]]);
-//#endregion
-export { Flame as t };
-
-//# sourceMappingURL=flame-BaiQWJ2t.js.map
